c528554cbf9fb2124ba1fb5489a0bdd68f8b2763
Joseph John <john71688@gmail.com>
2025-06-22 18:05:18 +0100

    mycommit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,8 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ['swiper/react', 'swiper/modules']
-  }
+  },
+  build: {
+    outDir: '../backend/frontend/dist', 
+  },
 });
